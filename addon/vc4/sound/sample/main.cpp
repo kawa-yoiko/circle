9b6a85cb721e3bcc08d8ad01b953946dc6a9679f
Shiqing <shiqing-thu18@yandex.com>
2019-11-25 13:04:46 +0800

    Move CKernel to main()

--- addon/vc4/sound/sample/main.cpp
+++ addon/vc4/sound/sample/main.cpp
@@ -17,31 +17,120 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-#include "kernel.h"
 #include <circle/startup.h>
+
+#include <circle/memory.h>
+#include <circle/actled.h>
+#include <circle/koptions.h>
+#include <circle/devicenameservice.h>
+#include <circle/screen.h>
+#include <circle/serial.h>
+#include <circle/exceptionhandler.h>
+#include <circle/interrupt.h>
+#include <circle/timer.h>
+#include <circle/logger.h>
+#include <circle/sched/scheduler.h>
+#include <vc4/vchiq/vchiqdevice.h>
+#include <vc4/sound/vchiqsounddevice.h>
+#include <circle/types.h>
+
+CMemorySystem		m_Memory;
+CActLED			m_ActLED;
+CKernelOptions		m_Options;
+CDeviceNameService	m_DeviceNameService;
+CScreenDevice		m_Screen (m_Options.GetWidth (), m_Options.GetHeight ());
+CSerialDevice		m_Serial;
+CExceptionHandler	m_ExceptionHandler;
+CInterruptSystem	m_Interrupt;
+CTimer			m_Timer (&m_Interrupt);
+CLogger			m_Logger (m_Options.GetLogLevel (), &m_Timer);
+CScheduler		m_Scheduler;
+
+CVCHIQDevice		m_VCHIQ (&m_Memory, &m_Interrupt);
+CVCHIQSoundDevice	m_VCHIQSound (&m_VCHIQ, (TVCHIQSoundDestination) m_Options.GetSoundOption ());
+
+static void Initialize ();
+static void Run ();
 
 int main (void)
 {
 	// cannot return here because some destructors used in CKernel are not implemented
+	Initialize ();
+	Run ();
+	while (1) { }
+}
 
-	CKernel Kernel;
-	if (!Kernel.Initialize ())
+#include "sound.h"
+
+#define SOUND_SAMPLES		(sizeof Sound / sizeof Sound[0] / SOUND_CHANNELS)
+
+static const char FromKernel[] = "kernel";
+
+void Initialize (void)
+{
+	m_ActLED.Blink (5);	// show we are alive
+
+	boolean bOK = TRUE;
+
+	if (bOK)
 	{
-		halt ();
-		return EXIT_HALT;
+		bOK = m_Screen.Initialize ();
 	}
-	
-	TShutdownMode ShutdownMode = Kernel.Run ();
 
-	switch (ShutdownMode)
+	if (bOK)
 	{
-	case ShutdownReboot:
-		reboot ();
-		return EXIT_REBOOT;
+		bOK = m_Serial.Initialize (115200);
+	}
 
-	case ShutdownHalt:
-	default:
-		halt ();
-		return EXIT_HALT;
+	if (bOK)
+	{
+		CDevice *pTarget = m_DeviceNameService.GetDevice (m_Options.GetLogDevice (), FALSE);
+		if (pTarget == 0)
+		{
+			pTarget = &m_Screen;
+		}
+
+		bOK = m_Logger.Initialize (pTarget);
+	}
+
+	if (bOK)
+	{
+		bOK = m_Interrupt.Initialize ();
+	}
+
+	if (bOK)
+	{
+		bOK = m_Timer.Initialize ();
+	}
+
+	if (bOK)
+	{
+		bOK = m_VCHIQ.Initialize ();
+	}
+}
+
+void Run (void)
+{
+	m_Logger.Write (FromKernel, LogNotice, "Compile time: " __DATE__ " " __TIME__);
+
+	while (1)
+	{
+		if (!m_VCHIQSound.Playback (Sound, SOUND_SAMPLES, SOUND_CHANNELS, SOUND_BITS))
+		{
+			m_Logger.Write (FromKernel, LogPanic, "Cannot start playback");
+		}
+
+		m_Logger.Write (FromKernel, LogNotice, "Playback started");
+
+		for (unsigned nCount = 0; m_VCHIQSound.PlaybackActive (); nCount++)
+		{
+			m_Screen.Rotor (0, nCount);
+
+			m_Scheduler.Yield ();
+		}
+
+		m_Logger.Write (FromKernel, LogNotice, "Playback completed");
+
+		m_Scheduler.Sleep (2);
 	}
 }
